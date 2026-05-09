e2d22d6a71c969134f7f20b64c836335f4274e43
danjiewu <danjiewu@hotmail.com>
2026-05-09 17:21:09 +0800

    更新测试项目

---
+++ LiteOrm.Tests/Expr/ExprVisitorTests.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using LiteOrm.Common;
+using Xunit;
+
+namespace LiteOrm.Common.UnitTests
+{
+    public class ExprVisitorTests
+    {
+        [Fact]
+        public void Visit_PreOrder_ShouldVisitParentBeforeChildren()
+        {
+            var visited = new List<string>();
+
+            ExprVisitor.Visit(node =>
+            {
+                visited.Add($"{node.ExprType}");
+                return true;
+            }, CreateSimpleExpr(), ExprVisitOrder.PreOrder);
+
+            Assert.Equal(
+            [
+                "LogicBinary",
+                "Property",
+                "Value"
+            ], visited);
+        }
+
+        [Fact]
+        public void Visit_PostOrder_ShouldVisitChildrenBeforeParent()
+        {
+            var visited = new List<string>();
+
+            ExprVisitor.Visit(node =>
+            {
+                visited.Add($"{node.ExprType}");
+                return true;
+            }, CreateSimpleExpr(), ExprVisitOrder.PostOrder);
+
+            Assert.Equal(
+            [
+                "Property",
+                "Value",
+                "LogicBinary"
+            ], visited);
+        }
+
+        [Fact]
+        public void Visit_Both_ShouldVisitEachNodeOnBothSides()
+        {
+            var visited = new List<string>();
+
+            ExprVisitor.Visit(node =>
+            {
+                visited.Add($"{node.ExprType}");
+                return true;
+            }, CreateSimpleExpr());
+
+            Assert.Equal(
+            [
+                "LogicBinary",
+                "Property",
+                "Property",
+                "Value",
+                "Value",
+                "LogicBinary"
+            ], visited);
+        }
+
+        private static LogicExpr CreateSimpleExpr()
+        {
+            return Expr.Prop("Age") > 18;
+        }
+    }
+}
