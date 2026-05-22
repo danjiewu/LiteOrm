bd823edf86ed0b932752c1948e6538f8d955cc11
danjiewu <danjiewu@hotmail.com>
2026-05-22 18:23:30 +0800

    AutoRegister默认为Singleton

--- LiteOrm.Common/Attributes/AutoRegisterAttribute.cs
+++ LiteOrm.Common/Attributes/AutoRegisterAttribute.cs
@@ -9,9 +9,9 @@ namespace LiteOrm.Common
     public class AutoRegisterAttribute : Attribute
     {
         /// <summary>
-        /// 服务生命周期，默认为Scoped
+        /// 服务生命周期，默认为Singleton
         /// </summary>
-        public Lifetime Lifetime { get; set; } = Lifetime.Scoped;
+        public Lifetime Lifetime { get; set; } = Lifetime.Singleton;
 
         /// <summary>
         /// 支持多个服务类型
